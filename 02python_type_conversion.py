c9e0634007c791a22d087d68f8648cdac2e792a6
rao-rabi <rabirao1039@gmail.com>
2023-09-16 13:14:24 +0500

    type conversion added

--- 02python_type_conversion.py
+++ 02python_type_conversion.py
@@ -1,7 +1,25 @@
-# Converts Pounds(lbs) to Kg's.
-
 user_weight = input("Enter your weight in pounds: ")
-# print(type(user_weight))
+print(type(user_weight))
+# explicit conversion(manual conversion)
 weight_in_kg = int(user_weight) * 0.45
+# implict conversion(automatic conversion)
 print("Your weight in Kg is: ", weight_in_kg)
-# print(type(weight_in_kg))
+print(type(weight_in_kg))
+
+# ord()---> used to convert char to int
+
+s = "a"
+c = ord(s)
+print(c)
+
+# hex()----> convert int to hexadecimal str
+
+n = 32;
+m = hex(n)
+print(m)
+
+# oct()----> convert int to octal str
+
+u = 32;
+v = oct(u)
+print(v)
