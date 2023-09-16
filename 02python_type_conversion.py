9634c8f01b6d66acb17dd4085c835513c55c9bed
rao-rabi <rabirao1039@gmail.com>
2023-09-16 12:55:34 +0500

    type conversion added

---
+++ 02python_type_conversion.py
@@ -0,0 +1,7 @@
+# Converts Pounds(lbs) to Kg's.
+
+user_weight = input("Enter your weight in pounds: ")
+# print(type(user_weight))
+weight_in_kg = int(user_weight) * 0.45
+print("Your weight in Kg is: ", weight_in_kg)
+# print(type(weight_in_kg))
