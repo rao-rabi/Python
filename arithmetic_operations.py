c197a03d7c601dddf492c8833c4d8baa5a5870f4
rao-rabi <rabirao1039@gmail.com>
2023-09-17 18:31:34 +0500

    arithematic operations added

---
+++ arithmetic_operations.py
@@ -0,0 +1,49 @@
+import math
+
+a = 5
+b = 10
+
+#addition
+
+print(a + b)
+
+#subtraction
+
+print(a - b)
+
+#multiplication
+
+print(a * b)
+
+#division
+
+print(a / b)  #returns in float
+print(a // b)  #returns in int
+
+#power
+
+print(a ** b)
+
+# argumented assignment operator
+
+u = 3
+
+u += 10
+print(u)
+u -= 10
+print(u)
+u *= 10
+print(u)
+u /= 10
+print(u)
+
+
+# math methods and module
+
+x = 2.5
+
+print(round(x))
+print(abs(-5.6))
+
+print(math.ceil(x))
+print(math.floor(x))
