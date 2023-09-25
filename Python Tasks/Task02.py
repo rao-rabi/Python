edb808d547685edbe06042d4813888552fea9315
rao-rabi <rabirao1039@gmail.com>
2023-09-25 10:18:25 +0500

    Task 02 added

---
+++ Python Tasks/Task02.py
@@ -0,0 +1,9 @@
+# 2. Write a program that converts pounds into kilograms. The program prompts the user
+# to enter a value in pounds, converts it to kilograms, and displays the result. One pound
+# is 0.454 kilograms.
+
+pounds = input("Enter weight in pounds: ")
+
+kilograms = int(pounds) * 0.454
+
+print(f"Weight In Kilograms is: {kilograms}")
